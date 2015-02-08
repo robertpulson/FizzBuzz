ac946331fb472d586a989510cb507f4ed3de836e
Phil Brockwell <nineteen40eight@gmail.com>
2015-02-08 20:56:18 +0000

    more refactoring

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -11,15 +11,13 @@ end
 
 def generate_numbers_with(limit)
   numbers = []
-  1.upto(limit) do |number|
-    numbers << fizz_buzz_says(number).to_s
-  end
+  1.upto(limit) { |number| numbers << fizz_buzz_says(number).to_s }
   numbers
 end
   
 def welcome_message
   print "\nWelcome"; sleep(1); print " to"; sleep(1); print " ."; sleep(1);
-  print "."; sleep(1); print "."; sleep(1); print " FizzBuzz!\n"; sleep(1)
+  print "."; sleep(1); print "."; sleep(1); print " FizzBuzz!\n"
 end
 
 def get_name
@@ -29,31 +27,22 @@ def get_name
 end
 
 def rules
-  puts "\nFizzBuzz is a group word game for children to teach them about division."
-  sleep(4)
-  puts "\nThe player designated to go first says the number '1', and each player thenceforth counts one number in turn."
-  sleep(4)
-  puts "\nHowever, any number divisible by three is replaced by the word 'fizz' and any divisible by five by the word 'buzz'."
-  sleep(4)
-  puts "\nNumbers divisible by both become 'fizzbuzz'."
-  sleep(4)
+  sleep(1); puts "\nFizzBuzz is a group word game for children to teach them about division."
+  sleep(4); puts "\nThe player designated to go first says the number '1', and each player thenceforth counts one number in turn."
+  sleep(4); puts "\nHowever, any number divisible by three is replaced by the word 'fizz' and any divisible by five by the word 'buzz'."
+  sleep(4); puts "\nNumbers divisible by both become 'fizzbuzz'."; sleep(4)
 end
 
 def get_limit_from(name)
   puts "\nOk #{name}. Enter the number you want to FizzBuzz upto..."
   limit = gets.chomp.to_i
-  # while limit == 0
-  #   puts "Come on #{name}, thats not a number!\nTry again..."
-  #   limit = gets.chomp.to_i
-  # end
 end
 
 def main(numbers, name)
   puts "\nOk #{name} we start at 1, you go first..."
   input = gets.chomp.downcase; i = 0
   while input == numbers[i]
-    sleep(1) 
-    puts numbers[i + 1]
+    sleep(1); puts numbers[i + 1]
     i += 2
     return true if i >= numbers.length
     input = gets.chomp.downcase
@@ -65,15 +54,14 @@ def ending(outcome, name)
   puts "Its a draw, would you like to play again #{name}?" if outcome == true
   puts "Haha you lost! Would you like to play again #{name}?" if outcome == false
   puts "Type y for yes, or n for no. Then hit Enter."
-  response = gets.chomp.downcase
-  play_with(name) if response == "y"
+  play_with(name) if gets.chomp.downcase == "y"
 end
 
 def play_with(name)
   limit    = get_limit_from(name)
   numbers  = generate_numbers_with(limit)
   outcome  = main(numbers, name)
-  response = ending(outcome, name)
+  ending(outcome, name)
 end
 
 def game_over
